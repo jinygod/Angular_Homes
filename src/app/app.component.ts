fc7aad3af2f3d900aefa1b0a3c412db6e67eefda
JeonSeongJin <ayclub8866@naver.com>
2023-07-14 10:16:01 +0900

    first aungluar settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [],
+  template: `<h1>Hello world!</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularboard';
+  title = 'homes';
 }
